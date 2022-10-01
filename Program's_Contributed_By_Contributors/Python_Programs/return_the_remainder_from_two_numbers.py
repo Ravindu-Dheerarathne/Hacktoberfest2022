42022209f0f2ff6a3e92c3564c1353f3658c16c7
“Ravindu <ravindu.first@gmail.com>
2022-10-01 07:50:10 +0530

    python file added

---
+++ Program's_Contributed_By_Contributors/Python_Programs/return_the_remainder_from_two_numbers.py
@@ -0,0 +1,9 @@
+#return the remainder from two numbers
+    
+num1 = float(input('Enter the first number : '))
+
+num2 = float(input('Enter the second number : '))
+
+remainder = num1%num2
+
+print('Remainder is : ',remainder)
